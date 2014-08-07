b1466a117f42c7cfa4bfb3418f1706a370dd1b28
konstantin <konstantin@rightscale.com>
2014-08-07 15:32:23 -0700

    Encode both: the string and the regexp (works for 98 out of 100 encodings including dummy ones)

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,31 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str =~ Regexp.new(SetEnc.encode('^.+$', str.encoding))
+  end
+end
+
+
+module SetEnc
+
+  # Encodes the given string with the given encoding (even for dummy encodings)
+  #
+  # There are 2 dummy encodings that fail to convert from 'UTF-8':
+  #  - ISO-2022-JP-2
+  #  - UTF-7
+  # In these 2 special cases it returns the original string back as it is.
+  #
+  # @param [String] str
+  # @param [Encoding] encoding
+  #
+  # @return [String] encoded str
+  #
+  # P.S. it changes the given string
+  #
+  def self.encode(str, encoding)
+    str.encode(encoding)
+  rescue Encoding::ConverterNotFoundError => e
+    puts "%s: %s" % [e.class.name, e.message]
+  ensure
+    return str
   end
 end
